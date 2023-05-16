e6bcf4c84e0562438070873765cdb7cef677d85d
Nicole <111709554+nicole440@users.noreply.github.com>
2023-05-16 11:52:14 -0400

    Add slideshow component with stock images for display on home view.

---
+++ TEnmo Vue/src/components/Slideshow.vue
@@ -0,0 +1,44 @@
+<template>
+<div class="slideshow">
+  <img src="src/assets/slideshow-photos/eugene-chystiakov-cvqvLMVEfBY-unsplash.jpg" alt="Slideshow Image">
+  <img src="src/assets/slideshow-photos/shawnanggg-H82HDdkcd4c-unsplash.jpg" alt="Slideshow Image">
+  <img src="src/assets/slideshow-photos/joel-muniz-0QuIgOjLLXc-unsplash.jpg" alt="Slideshow Image">
+  <img src="src/assets/slideshow-photos/ghen-mar-cuano-R6dSBkz32B8-unsplash.jpg" alt="Slideshow Image">
+  <img src="src/assets/slideshow-photos/elevate-UoPNA8I-_p0-unsplash.jpg" alt="Slideshow Image">
+  <img src="src/assets/slideshow-photos/drew-beamer-bTN-zKFy9uA-unsplash.jpg" alt="Slideshow Image">
+</div>
+</template>
+
+<style>
+.slideshow {
+  position: relative;
+  width: 100%;
+  height: 500px;
+  overflow: hidden;
+}
+
+.slideshow img {
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 25%;
+  height: 100%;
+  opacity: 0;
+  animation: fadeAnimation 15s infinite;
+}
+
+@keyframes fadeAnimation {
+  0% {
+    opacity: 0;
+  }
+  20% {
+    opacity: 1;
+  }
+  80% {
+    opacity: 1;
+  }
+  100% {
+    opacity: 0;
+  }
+}
+</style>
--- TEnmo Vue/src/views/HomeView.vue
+++ TEnmo Vue/src/views/HomeView.vue
@@ -1,9 +1,11 @@
 <script setup>
 import Home from '../components/Home.vue'
+import Slideshow from '../components/Slideshow.vue'
 </script>
 
 <template>
   <div>
     <Home />
+    <Slideshow />
   </div>
 </template>
