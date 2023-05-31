a5d0643fd1338d91ca0e01688e1a80a6fda064b4
Nicole <111709554+nicole440@users.noreply.github.com>
2023-05-31 15:09:30 -0400

    Update database schema

--- database/schema.sql
+++ database/schema.sql
@@ -1,20 +1,7 @@
 BEGIN TRANSACTION;
 
-DROP TABLE IF EXISTS transfer, account, tenmo_user, transfer_type, transfer_status;
+DROP TABLE IF EXISTS transfer, account, tenmo_user;
 DROP SEQUENCE IF EXISTS seq_user_id, seq_account_id, seq_transfer_id;
-
-
-CREATE TABLE transfer_type (
-	transfer_type_id serial NOT NULL,
-	transfer_type_desc varchar(10) NOT NULL,
-	CONSTRAINT PK_transfer_type PRIMARY KEY (transfer_type_id)
-);
-
-CREATE TABLE transfer_status (
-	transfer_status_id serial NOT NULL,
-	transfer_status_desc varchar(10) NOT NULL,
-	CONSTRAINT PK_transfer_status PRIMARY KEY (transfer_status_id)
-);
 
 CREATE SEQUENCE seq_user_id
   INCREMENT BY 1
@@ -27,7 +14,7 @@ CREATE TABLE tenmo_user (
 	first_name varchar(20) NOT NULL,
 	last_name varchar(20) NOT NULL,
 	email varchar(50) UNIQUE NOT NULL,
-	phone varchar(10) UNIQUE,
+	phone varchar(10),
 	password_hash varchar(200) NOT NULL,
 	profile_picture varchar(200),
 	role varchar(20),
@@ -57,25 +44,16 @@ CREATE SEQUENCE seq_transfer_id
 
 CREATE TABLE transfer (
 	transfer_id int NOT NULL DEFAULT nextval('seq_transfer_id'),
-	transfer_type_id int NOT NULL,
-	transfer_status_id int NOT NULL,
+	transfer_type VARCHAR(30) NOT NULL,
+	transfer_status VARCHAR(30) NOT NULL,
 	account_from int NOT NULL,
 	account_to int NOT NULL,
 	amount decimal(13, 2) NOT NULL,
 	CONSTRAINT PK_transfer PRIMARY KEY (transfer_id),
 	CONSTRAINT FK_transfer_account_from FOREIGN KEY (account_from) REFERENCES account (account_id),
 	CONSTRAINT FK_transfer_account_to FOREIGN KEY (account_to) REFERENCES account (account_id),
-	CONSTRAINT FK_transfer_transfer_status FOREIGN KEY (transfer_status_id) REFERENCES transfer_status (transfer_status_id),
-	CONSTRAINT FK_transfer_transfer_type FOREIGN KEY (transfer_type_id) REFERENCES transfer_type (transfer_type_id),
 	CONSTRAINT CK_transfer_not_same_account CHECK (account_from <> account_to),
 	CONSTRAINT CK_transfer_amount_gt_0 CHECK (amount > 0)
 );
-
-INSERT INTO transfer_status (transfer_status_desc) VALUES ('Pending');
-INSERT INTO transfer_status (transfer_status_desc) VALUES ('Approved');
-INSERT INTO transfer_status (transfer_status_desc) VALUES ('Rejected');
-
-INSERT INTO transfer_type (transfer_type_desc) VALUES ('Request');
-INSERT INTO transfer_type (transfer_type_desc) VALUES ('Send');
 
 COMMIT;
